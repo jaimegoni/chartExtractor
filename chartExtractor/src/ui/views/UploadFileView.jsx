62ab040b925fb15db649dc7c3503a12d837b78d8
jaime <jaimegonicarnicero@hotmail.com>
2023-03-11 18:15:11 +0100

    Some changes on styles

--- chartExtractor/src/ui/views/UploadFileView.jsx
+++ chartExtractor/src/ui/views/UploadFileView.jsx
@@ -4,18 +4,41 @@ import { StandardView } from "../templates/StandardView/StandardView";
 
 export const UploadFileView = ()=>{
 
+    const [chartName, setChartName] = useState("");
     const [file, setFile] = useState(null);
+
+    const isInformationUploaded = ()=>{
+        if (!(chartName === "") && !(file === null)){
+            return true;
+        }
+        return false;
+    }
 
     return(
         <StandardView>
             <>  
-                <h1>Name of the chart</h1>
+                <h1>1) Name of the chart</h1>
                 <br/>
-                <h1>Choose a file</h1>
+                <input type="text" placeholder="Chart name" onChange={(event) => {setChartName(event.target.value);}}/>
+                <br/><br/>
+                <h1>2) Choose a file</h1>
+                <br/>
                 <FileUploader
                     file={file}
                     setFile={setFile}
                 />
+                <br/><br/>
+                {
+                    isInformationUploaded()
+                        &&
+                    <>
+                        <h1>3) Continue</h1>
+                        <br/>
+                        <button className="btn btn-primary">Next</button>
+                    </>
+                    
+                }
+
             </>
         </StandardView>
     )
