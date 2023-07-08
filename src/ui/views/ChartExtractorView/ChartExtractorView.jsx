04558798acd6cec1e068611c0a6ffd7265aaec2e
jaime <jaimegonicarnicero@hotmail.com>
2023-07-08 10:24:45 +0200

    ChartExtractorView modified. Next steps: show magnifier glass, add axis selection

--- src/ui/views/ChartExtractorView/ChartExtractorView.jsx
+++ src/ui/views/ChartExtractorView/ChartExtractorView.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
+import './ChartExtractorView.css';
+
 import { StandardView } from "../../templates/StandardView/StandardView";
 import { getStoredChartByKey } from "../../../core/services/ChartsRegister/GetStoredChartByKey";
 
@@ -8,16 +10,31 @@ export const ChartExtractorView = ()=>{
 
     const { chartKey } = useParams();
 
-    const chartData = getStoredChartByKey(chartKey);
+    const [chartData, setChartData] = useState(getStoredChartByKey(chartKey));
+    const [{displayWidth, displayHeight}, setDisplayWidth] = useState({
+        displayWidth: chartData.imageWidth,
+        displayHeight: chartData.imageHeight
+    });
 
-    useEffect(() => {
-      console.log({ chartKey });
-    }, [chartKey]);
+    const modifyDisplayWidth = (event)=>{
+        
+        setDisplayWidth({
+            displayWidth: event.target.value,
+            displayHeight: Math.round(event.target.value * chartData.imageHeight / chartData.imageWidth)
+        })
+
+    }
 
     return(
         <StandardView>
-            <h1>Params: {chartData.chartName}</h1>
-            <img src={chartData.b64image} alt="chartImage"/>
+            <div className="page__container--div">
+                <h1>Chart name: {chartData.chartName}</h1>
+                <br/>
+                <label>Chart display width:</label>
+                <input type="number" value={displayWidth} onChange={modifyDisplayWidth}/>
+                <br/>
+                <img className="chart__image--img" src={chartData.b64image} alt="chartImage" style={{width: displayWidth, height:displayHeight}}/>
+            </div>
         </StandardView>
     )
 }
